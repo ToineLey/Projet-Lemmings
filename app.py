a5d94ddf62d90af0796faee9fbb25d30c1ba4591
ToineLEy <toineleydert@gmail.com>
2024-05-14 09:44:26 +0200

    yay

---
+++ app.py
@@ -0,0 +1,20 @@
+from Jeu import *
+import pyxel
+
+
+class Interface:
+    CREUSE=0
+    STOP=1
+    REDEMARRE=2
+
+class App:
+    def __init__(self) -> None:
+        boutons= 'Interface()'
+        game = 'Jeu()'
+        pyxel.mouse(True)
+    def onclick():
+        pass
+    def draw():
+        pass
+    def update():
+        pass
