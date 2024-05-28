61b12a544844c290bdac3ec97d3e0ed3c6825346
Jules <julesaudouin64@gmail.com>
2024-05-28 08:51:02 +0200

    ajouts lemmings, niveau et modifs grille

--- niveau.py
+++ niveau.py
@@ -16,3 +16,14 @@ class Niveau:
 
     def calcul_score(self):
         pass
+
+    def tour_lemmings(self, lem):
+        # à compléter
+        if self.grille.case(lem.x, lem.y).terrain == Type_t.VIDE:
+            lem.etat = Etats.MORT
+        elif tuple(lem.x, lem.y) == self.coo_arr:
+            lem.etat = Etats.VICTOIRE
+        if Etats.MARCHER_1 <= lem.etat <= Etats.MARCHER_3 or lem.etat == Etats.CHUTE:
+            lem.deplacement(self.grille)
+        elif lem.etat == Etats.CREUSER_1 or lem.etat == Etats.CREUSER_2:
+            lem.creuse(self.grille)
