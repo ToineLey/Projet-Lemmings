48da3c689d1444cc63586e484c0ff87984cf3c13
Lunosome <rvl.arg@gmail.com>
2024-06-03 14:34:18 +0200

    modification du fichier json et de ma partie sur niveau

--- niveau.py
+++ niveau.py
@@ -1,15 +1,22 @@
 from lemming import *
 from grille import Grille
+import pyxel as pyx
+import json
+
+
+
 
 class Niveau:
-    def __init__(self):
-        nb_niveau = int() # correspond u niveau de la tilmap
+    def __init__(self,num_niv):
+        self.num_niveau = num_niv # correspond u niveau de la tilmap
         grille = 'Grille()'
+        taille_gr = ()
         nb_lem_dep = int()
         nb_lem_arr = int()
         coo_dep = tuple()
         coo_arr = tuple()
         lem_IGM = set()
+
 
     def apparition(self):
         self.lem_IGM.add(Lemming(*self.coo_dep))
@@ -27,3 +34,17 @@ class Niveau:
             lem.deplacement(self.grille)
         elif lem.etat == Etats.CREUSER_1 or lem.etat == Etats.CREUSER_2:
             lem.creuse(self.grille)
+
+    """def charger_nv(self):
+        for i in range(self.len_x):
+            for j in range(self.len_y):
+                    tab[i][j] = Dict_Test[pyx.tilemap(0).pget(i*2,j*2)]"""
+    
+    def charger_json(self):
+
+        if self.num_niv < 1:
+            return ValueError
+        else:
+            with open('niveaux.json') as file:
+                tab_json = json.load(file)
+
