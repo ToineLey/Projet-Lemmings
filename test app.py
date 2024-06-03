7a5a9ee7006563db98242e7bc26119af4fef02a0
ToineLEy <toineleydert@gmail.com>
2024-06-03 15:07:06 +0200

    ajout de la classe app

--- test app.py
+++ test app.py
@@ -1,30 +1,27 @@
 from jeu import *
 import pyxel as pyx
-import time
-
-H,L = 17*8,32*8
-y_interface=H-16
-t = [[None]*3 for _ in range(4)]
-
 class App:
-    def __init__(self,longeur_x,longeur_y):
-        self.len_x = longeur_x
-        self.len_y = longeur_y
-        pyx.init(L,H,title= 'Tst')
+    def __init__(self,h,l):
+        self.H,self.L = (h+1)*8,l*8
+        self.y_interface=self.H-16
+        pyx.init(self.L,self.H,title= 'Tst')
         self.x = 0
         self.apu=1
-        self.t=True
-        self.t2=True
-        self.t3=True
+        self.bouton_1=True
+        self.bouton_2=True
+        self.bouton_3=True
+        self.fc1=0
+        self.fc2=0
+        self.fc3=0
         self.b1,self.b2,self.b3=0,16,32
         pyx.load("lemmings.pyxres")
-        self.get_tilemap(t)
         # Lancement du jeu
         pyx.run(self.update, self.draw)
         
             
     def update(self):
         self.onclick()
+        self.wait()
 
     def draw(self):
        pyx.cls(self.apu)
@@ -33,35 +30,39 @@ class App:
 
     def onclick(self):
         if pyx.btnp(pyx.MOUSE_BUTTON_LEFT,True,False):
-            if pyx.mouse_x>=self.b1 and pyx.mouse_x<self.b1+16 and pyx.mouse_y>=y_interface and pyx.mouse_y<H:
-                if self.t:
-                    self.t=False
-                elif not self.t:
-                    self.t=True
-            elif pyx.mouse_x>=self.b2 and pyx.mouse_x<self.b2+16 and pyx.mouse_y>=y_interface and pyx.mouse_y<H:
-                if self.t2:
-                    self.t2=False
-                elif not self.t2:
-                    self.t2=True
-            elif pyx.mouse_x>=self.b3 and pyx.mouse_x<self.b3+16 and pyx.mouse_y>=y_interface and pyx.mouse_y<H:
-                if self.t3:
-                    self.t3=False
-                elif not self.t3:
-                    self.t3=True
+            if pyx.mouse_x>=self.b1 and pyx.mouse_x<self.b1+16 and pyx.mouse_y>=self.y_interface and pyx.mouse_y<self.H:
+                if self.bouton_1:
+                    self.bouton_1=False
+                    self.fc1=pyx.frame_count
+            elif pyx.mouse_x>=self.b2 and pyx.mouse_x<self.b2+16 and pyx.mouse_y>=self.y_interface and pyx.mouse_y<self.H:
+                if self.bouton_2:
+                    self.bouton_2=False
+                    self.fc2=pyx.frame_count
+            elif pyx.mouse_x>=self.b3 and pyx.mouse_x<self.b3+16 and pyx.mouse_y>=self.y_interface and pyx.mouse_y<self.H:
+                if self.bouton_3:
+                    self.bouton_3=False
+                    self.fc3=pyx.frame_count
         
+    def wait(self):
+        if pyx.frame_count==self.fc1+5:
+            self.bouton_1=True
+        if pyx.frame_count==self.fc2+5:
+            self.bouton_2=True
+        if pyx.frame_count==self.fc3+5:
+            self.bouton_3=True
+    
     def bouton(self):
-        if self.t:
-            pyx.blt(self.b1,y_interface,0,0,80,16,16)
-        if self.t2:
-            pyx.blt(self.b2,y_interface,0,16,80,16,16)
-        if self.t3:
-            pyx.blt(self.b3,y_interface,0,32,80,16,16)
+        if self.bouton_1:
+            pyx.blt(self.b1,self.y_interface,0,0,80,16,16)
+        if self.bouton_2:
+            pyx.blt(self.b2,self.y_interface,0,16,80,16,16)
+        if self.bouton_3:
+            pyx.blt(self.b3,self.y_interface,0,32,80,16,16)
 
-    def get_tilemap(self,tab):
-        # parcourir une tilemap et recupérer les tuples en les metants dans un tableau de tableau de meme taille. 
-        # Taille fixé à , 7.7 pour les testes.
-        for i in range(self.len_y):
-            for j in range(self.len_x):
-                tab[i][j] = pyx.tilemap(0).pget(i*2,j*2)
+App(16,32)
 
-App(3,4)
+
+
+
+
+
